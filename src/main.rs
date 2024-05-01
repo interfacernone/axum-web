96bb3203e3d2b52148396a69fcd148f05ba89568
liuziling <18844115361@163.com>
2024-05-01 17:56:40 +0800

    连接本地pg

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
-use rand::Rng;
-use std::{cmp::Ordering, io};
+use postgres::Client;
+use postgres::NoTls;
 fn main() {
-    //生成随机数
-    let random = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        //输入变量是一个String
-        let mut input: String = String::new();
-        //接受输入
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-        let guess: u32 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        
-        println!("You guessed: {guess}");
-        match guess.cmp(&random) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                print!("You win!");
-                break;
-            }
-        }
-    }
+    #[warn(unused_mut)]
+    let mut client= Client::connect("postgresql://liuziling:a2sh6817@localhost/pgdb", NoTls);
 }
